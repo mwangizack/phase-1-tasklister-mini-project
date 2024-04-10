4745285b60b03fdb4923112ad8b3a53330fe1e02
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-04-10 19:34:19 +0300

    Add displaying and deleting of todo items features

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  const form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const insertedTask = form.new_task_description.value;
+    const newTask = document.createElement("li");
+    const doneButton = document.createElement('button');
+    doneButton.innerHTML = '&#10004'
+
+    newTask.textContent = insertedTask;
+    doneButton.style.marginLeft = '10px';
+    newTask.appendChild(doneButton);
+
+    document.querySelector("#tasks").appendChild(newTask);
+    form.reset()
+
+    doneButton.addEventListener('click', (e) => {
+      e.target.parentNode.remove()
+    })
+  });
 });
